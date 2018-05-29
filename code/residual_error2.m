483dc4f0d09c82d0db298f0e6fd6384fa54aabc9
Ashutosh Ahmad Alexandar <ashutoshahmadalexandar@Ashutoshs-MacBook-Pro.local>
2018-05-29 15:50:47 -0700

    first commit

---
+++ code/residual_error2.m
@@ -0,0 +1,8 @@
+function residuals = residual_error2(F, matches)
+   
+    numMatches = size(matches,1);
+    L = (F * [matches(:,1:2) ones(numMatches,1)]')';  
+    L = L ./ repmat(sqrt(L(:,1).^2 + L(:,2).^2), 1, 3); 
+    distances = sum(L .* [matches(:,3:4) ones(numMatches,1)],2); 
+    residuals = abs(distances);
+end
